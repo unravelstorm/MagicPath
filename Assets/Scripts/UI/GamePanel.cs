ca15166d936da24dbd30268142737cb1156893d6
ooo <unravelstorm@outlook.com>
2019-02-12 17:56:16 +0800

    first

---
+++ Assets/Scripts/UI/GamePanel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePanel : MonoBehaviour {
+    private Button btn_Pause;
+    private Button btn_Play;
+    private Text txt_DiamondCount;
+    private Text txt_Score;
+
+    private void Init()
+    {
+        btn_Pause = transform.Find("btn_pause").GetComponent<Button>();
+        btn_Pause.onClick.AddListener(OnPauseButtonClick);
+        btn_Play = transform.Find("btn_play").GetComponent<Button>();
+        btn_Play.onClick.AddListener(OnPlayButtonClick);
+        txt_DiamondCount = transform.Find("Diamond/txt_diamondCount").GetComponent<Text>();
+        txt_Score = transform.Find("txt_score").GetComponent<Text>();
+        btn_Play.gameObject.SetActive(false);
+        gameObject.SetActive(false);
+    }
+    private void Awake()
+    {
+        EventCenter.AddListener(EventDefine.ShowGamePanel, Show);
+        Init();
+    }
+    private void OnDestroy()
+    {
+        EventCenter.RemoveListener(EventDefine.ShowGamePanel, Show);
+    }
+    /// <summary>
+    /// 显示此面板
+    /// </summary>
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    /// <summary>
+    /// 暂停按钮点击
+    /// </summary>
+    private void OnPauseButtonClick()
+    {
+        btn_Play.gameObject.SetActive(true);
+        btn_Pause.gameObject.SetActive(false);
+        //游戏暂停
+
+    }
+    /// <summary>
+    /// 开始按钮点击
+    /// </summary>
+    private void OnPlayButtonClick()
+    {
+        btn_Play.gameObject.SetActive(false);
+        btn_Pause.gameObject.SetActive(true);
+        //继续游戏
+
+    }
+}
---
+++ Assets/Scripts/UI/MainPanel.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainPanel : MonoBehaviour {
+    private Button btnStart;
+    private Button btnShop;
+    private Button btnRank;
+    private Button btnSound;
+
+    private void Init()
+    {
+        //绑定按钮
+        btnStart = transform.Find("btn_start").GetComponent<Button>();
+        //绑定按钮点击函数
+        btnStart.onClick.AddListener(OnStartButtonClick);
+
+        btnShop = transform.Find("Buttons/btn_shop").GetComponent<Button>();
+        btnShop.onClick.AddListener(OnShopButtonClick);
+
+        btnRank = transform.Find("Buttons/btn_rank").GetComponent<Button>();
+        btnRank.onClick.AddListener(OnRankButtonClick);
+
+        btnSound = transform.Find("Buttons/btn_sound").GetComponent<Button>();
+        btnSound.onClick.AddListener(OnSoundButtonClick);
+    }
+
+    private void Awake()
+    {
+        Init();
+    }
+    /// <summary>
+    /// 开始按钮点击后调用
+    /// </summary>
+    private void OnStartButtonClick()
+    {
+        EventCenter.Broadcast(EventDefine.ShowGamePanel);
+        gameObject.SetActive(false);
+    }
+    /// <summary>
+    /// 商店按钮点击事件
+    /// </summary>
+    private void OnShopButtonClick()
+    {
+
+    }
+    /// <summary>
+    /// 排行榜按钮点击事件
+    /// </summary>
+    private void OnRankButtonClick()
+    {
+
+    }
+    /// <summary>
+    /// 声音按钮点击事件  
+    /// </summary>
+    private void OnSoundButtonClick()
+    {
+
+    }
+}
